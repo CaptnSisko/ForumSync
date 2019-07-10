42dde735a0acdcc80efe88493b8ca39d70fec838
CaptnSisko <trevor8568@gmail.com>
2019-07-09 20:54:33 -0400

    Signing test

--- src/main/java/me/sisko/forumsync/Main.java
+++ src/main/java/me/sisko/forumsync/Main.java
@@ -8,9 +8,10 @@ import java.sql.SQLException;
 import me.sisko.commands.RegisterCommand;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class Main extends Plugin implements net.md_5.bungee.api.plugin.Listener {
+public class Main extends Plugin implements Listener {
 	private static Connection connection;
 	private static Main plug;
 	private static PermissionManager perms;
